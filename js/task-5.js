c48060f0f6177ba341e5da00fd4ba3435093b0f9
Katya Kondratchuk <68007177+Katya32@users.noreply.github.com>
2021-07-16 16:28:38 +0300

    change task #5

--- js/task-5.js
+++ js/task-5.js
@@ -2,7 +2,7 @@ const inputRef = document.querySelector("#name-input"); //ссылка на inpu
 const nameRef = document.querySelector("#name-output"); //ссылка span с именем
 
 const changeName = e => {
-nameRef.textContent = e.currentTarget.value; //заменяем текст в span текстом, введенным в инпуте
+nameRef.textContent = e.currentTarget.value || 'незнакомец'; //заменяем текст в span текстом, введенным в инпуте
 }
 
 inputRef.addEventListener("input", changeName);
